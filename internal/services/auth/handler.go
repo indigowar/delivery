6b4e3c4c7c08d3180dd1e4b42b74251bbf466d3d
Max Kamerov <indigowar.github@gmail.com>
2024-01-08 22:01:00 +0300

    modified auth handler

--- internal/services/auth/handler.go
+++ internal/services/auth/handler.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"errors"
+	"log"
+	"net/http"
+
 	"github.com/alexedwards/scs/v2"
 	"github.com/labstack/echo/v4"
 )
@@ -33,9 +37,38 @@ func (h *Handler) ServeRegistrationPage(handle string) echo.HandlerFunc {
 }
 
 func (h *Handler) HandleLoginRequest() echo.HandlerFunc {
-	return func(c echo.Context) error { return nil }
+	return func(c echo.Context) error {
+		phone := c.FormValue("phone")
+		password := c.FormValue("password")
+
+		id, err := h.service.Login(c.Request().Context(), phone, password)
+		if err != nil {
+			if errors.Is(err, ErrCredentialsAreInvalid) {
+				return c.NoContent(http.StatusBadRequest)
+			}
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		h.sm.Put(c.Request().Context(), "user-id", id.String())
+		return c.Redirect(http.StatusSeeOther, "/")
+	}
 }
 
 func (h *Handler) HandleRegisterRequest() echo.HandlerFunc {
-	return func(c echo.Context) error { return nil }
+	return func(c echo.Context) error {
+		phone := c.FormValue("phone")
+		password := c.FormValue("password")
+
+		id, err := h.service.Register(c.Request().Context(), phone, password)
+		if err != nil {
+			log.Println(err)
+			if errors.Is(err, ErrCredentialsAreInvalid) || errors.Is(err, ErrAlreadyInUse) {
+				return c.NoContent(http.StatusBadRequest)
+			}
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		h.sm.Put(c.Request().Context(), "user-id", id.String())
+		return c.Redirect(http.StatusSeeOther, "/")
+	}
 }
