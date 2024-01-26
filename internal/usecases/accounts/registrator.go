c6f9ff8edc743719bdc901ea2d030fe3d69bcce0
Max Kamerov <indigowar.github@gmail.com>
2024-01-26 15:06:41 +0300

    implemented accounts.Registrator

--- internal/usecases/accounts/registrator.go
+++ internal/usecases/accounts/registrator.go
@@ -2,8 +2,12 @@ package accounts
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	"github.com/google/uuid"
+
+	"github.com/indigowar/delivery/internal/entities"
 )
 
 // registrator - is an implementation of Registrator interface
@@ -13,8 +17,23 @@ type registrator struct {
 
 // RegisterAccount implements Registrator
 func (r *registrator) RegisterAccount(ctx context.Context, phone string, password string) (uuid.UUID, error) {
-	// todo: implement
-	panic("unimplemented")
+	account, err := entities.NewAccount(phone, password)
+	if err != nil {
+		return uuid.UUID{}, ErrInvalidCredentials
+	}
+
+	account, err = r.storage.Add(ctx, account)
+	if err != nil {
+		if errors.Is(err, ErrStorageAlreadyExists) {
+			return uuid.UUID{}, ErrAccountIsAlreadyExists
+		}
+
+		log.Println(err)
+
+		return uuid.UUID{}, err
+	}
+
+	return account.ID(), nil
 }
 
 func NewRegistrator(storage Storage) Registrator {
