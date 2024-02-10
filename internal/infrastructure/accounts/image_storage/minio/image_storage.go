d09b95d8bd76f2fa5e174b11aa3a6b66bd6f4e43
Max Kamerov <indigowar.github@gmail.com>
2024-02-10 12:56:32 +0300

    made image storage using minio for profile pictures

--- internal/infrastructure/accounts/image_storage/minio/image_storage.go
+++ internal/infrastructure/accounts/image_storage/minio/image_storage.go
@@ -1,8 +1,11 @@
 package minio
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 
@@ -16,7 +19,23 @@ type ImageStorage struct {
 }
 
 func (is *ImageStorage) Add(ctx context.Context, image []byte) (*url.URL, error) {
-	panic("not implemented")
+	objectName := fmt.Sprintf("image_%d.jpg", time.Now().UnixNano())
+
+	_, err := is.client.PutObject(ctx, is.backet, objectName, bytes.NewReader(image), int64(len(image)), minio.PutObjectOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to save image: %w", err)
+	}
+
+	expire := 100 * 365 * 24 * time.Hour
+
+	reqParams := make(url.Values)
+
+	url, err := is.client.PresignedGetObject(ctx, is.backet, objectName, expire, reqParams)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get saved image: %w", err)
+	}
+
+	return url, nil
 }
 
 func NewImageStorage(host string, port int, user string, password string) (*ImageStorage, error) {
