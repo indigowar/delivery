be38905d69d327198799b368f1a70b60d5e5d4a3
Max Kamerov <indigowar.github@gmail.com>
2024-02-11 17:52:28 +0300

    implemented rest delivery for accounts registrator

--- internal/infrastructure/accounts/delivery/rest/handlers/registrate.go
+++ internal/infrastructure/accounts/delivery/rest/handlers/registrate.go
@@ -1,15 +1,62 @@
 package handlers
 
 import (
-	"github.com/indigowar/delivery/internal/usecases/accounts"
+	"errors"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+
+	"github.com/indigowar/delivery/internal/usecases/accounts"
 )
 
-type registrationRequest struct{}
-type registrationResponse struct{}
+type registrationRequest struct {
+	Phone    string `json:"phone"`
+	Password string `json:"password"`
+}
+
+type registrationResponse struct {
+	ID string `json:"id"`
+}
+
+type registrationErrorResponse struct {
+	Error string `json:"error"`
+	Msg   string `json:"msg"`
+}
 
 func RegistrationHandler(svc accounts.Registrator) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		request := registrationRequest{}
+		if err := c.Bind(&request); err != nil {
+			return c.JSON(http.StatusBadRequest, registrationErrorResponse{
+				Error: "request data is invalid",
+				Msg:   "failed to bind data to request structure",
+			})
+		}
+
+		id, err := svc.RegisterAccount(c.Request().Context(), request.Phone, request.Password)
+		if err != nil {
+			if errors.Is(err, accounts.ErrAccountIsAlreadyExists) {
+				return c.JSON(http.StatusBadRequest, registrationErrorResponse{
+					Error: "account already exists",
+					Msg:   "account with provided phone already  exists.",
+				})
+			}
+
+			if errors.Is(err, accounts.ErrProvidedDataIsInvalid) {
+				return c.JSON(http.StatusBadRequest, registrationErrorResponse{
+					Error: "data is invalid",
+					Msg:   "provided data is invalid",
+				})
+			}
+
+			return c.JSON(http.StatusInternalServerError, registrationErrorResponse{
+				Error: "internal",
+				Msg:   "internal server error occurred",
+			})
+		}
+
+		return c.JSON(http.StatusCreated, registrationResponse{
+			ID: id.String(),
+		})
 	}
 }
