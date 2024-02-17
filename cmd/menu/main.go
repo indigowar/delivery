be64c3acc4904de6965a57c36210e8c02a383653
Max Kamerov <indigowar.github@gmail.com>
2024-02-17 11:19:21 +0300

    added menu database to services

--- cmd/menu/main.go
+++ cmd/menu/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 
 	"github.com/indigowar/delivery/pkg/http/status"
+	"github.com/indigowar/delivery/pkg/postgres"
 )
 
 func main() {
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DB")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	_, err := postgres.Connect(host, port, user, password, dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := echo.New()
 
 	router.GET("/status", status.StatusHandler("menu"))
