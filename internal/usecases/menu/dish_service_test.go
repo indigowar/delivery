a2cca7887992c9e2ecf0e8c463a56a28c78470c4
Max Kamerov <indigowar.github@gmail.com>
2024-02-22 14:58:16 +0300

    made tests for menu usecases

--- internal/usecases/menu/dish_service_test.go
+++ internal/usecases/menu/dish_service_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"net/url"
 	"testing"
 
 	"github.com/google/uuid"
@@ -42,7 +43,9 @@ func (suite *DishServiceTestSuite) TestGetWhenStorageEmpty() {
 	dish, err := suite.service.Get(context.Background(), uuid.New())
 
 	suite.Nil(dish, "DishService.Get should return nil as entity, when the entity is not found in storage")
-	suite.ErrorIsf(err, ErrDishNotFound, "DishService.Get should return ErrDishNotFound as an error, instead of %s", err)
+	if suite.NotNil(err, "DishService.Get should return an error") {
+		suite.ErrorIsf(err, ErrDishNotFound, "DishService.Get should return ErrDishNotFound as an error, instead of %s", err)
+	}
 }
 
 func (suite *DishServiceTestSuite) TestGetWhenStorageHasUnexpectedError() {
@@ -53,7 +56,102 @@ func (suite *DishServiceTestSuite) TestGetWhenStorageHasUnexpectedError() {
 	dish, err := suite.service.Get(context.Background(), uuid.New())
 
 	suite.Nil(dish, "DishService.Get should return nil as entity, when the unexpected error returned from the storage")
-	suite.ErrorIsf(err, ErrDishNotFound, "DishService.Get should return ErrInternalServerError as an error, instead of %s", err)
+	if suite.NotNil(err, "DishService.Get should return an error") {
+		suite.ErrorIsf(err, ErrDishNotFound, "DishService.Get should return ErrInternalServerError as an error, instead of %s", err)
+	}
+}
+
+func (suite *DishServiceTestSuite) TestCreateWhenImageStorageCantSaveImage() {
+	suite.imageStorage.AddFunc = func(ctx context.Context, image []byte) (*url.URL, error) {
+		return nil, errors.New("failed to save")
+	}
+
+	suite.dishStorage.AddFunc = func(ctx context.Context, dish *entities.Dish) (*entities.Dish, error) {
+		return dish, nil
+	}
+
+	createInfo := DishInfo{
+		Name:  "soup",
+		Price: 60.0,
+		Ingredients: []string{
+			"water",
+			"carrot",
+			"cabbage",
+		},
+	}
+
+	image := make([]byte, 10)
+
+	dish, err := suite.service.Create(context.Background(), &createInfo, image)
+
+	suite.Nil(dish, "DishService.Create should return nil as entity, when the ImageStorage can't save the image")
+	if suite.NotNil(err, "DishService.Create should return an error") {
+		suite.ErrorIsf(err, ErrInternalServerError, "DishService.Create should return ErrInternalServerError as an error, instead of %s", err)
+	}
+}
+
+func (suite *DishServiceTestSuite) TestCreateWhenDishStorageHasUnexpectedError() {
+	suite.imageStorage.AddFunc = func(ctx context.Context, image []byte) (*url.URL, error) {
+		return url.Parse("http://valid_url.com")
+	}
+
+	suite.dishStorage.AddFunc = func(ctx context.Context, dish *entities.Dish) (*entities.Dish, error) {
+		return nil, errors.New("unexpected error")
+	}
+
+	createInfo := DishInfo{
+		Name:  "soup",
+		Price: 60.0,
+		Ingredients: []string{
+			"water",
+			"carrot",
+			"cabbage",
+		},
+	}
+
+	image := make([]byte, 10)
+
+	dish, err := suite.service.Create(context.Background(), &createInfo, image)
+
+	suite.Nil(dish, "DishService.Create should return nil as entity, when DishStorage can't save the dish info")
+	if suite.NotNil(err, "DishService.Create should return an error") {
+		suite.ErrorIsf(err, ErrInternalServerError, "DishService.Create should return ErrInternalServerError as an error, instead of %s", err)
+	}
+}
+
+func (suite *DishServiceTestSuite) TestCreateValid() {
+	validUrl, _ := url.Parse("http://valid_url.com")
+
+	suite.imageStorage.AddFunc = func(ctx context.Context, image []byte) (*url.URL, error) {
+		return validUrl, nil
+	}
+
+	suite.dishStorage.AddFunc = func(ctx context.Context, dish *entities.Dish) (*entities.Dish, error) {
+		return dish, nil
+	}
+
+	createInfo := DishInfo{
+		Name:  "soup",
+		Price: 60.0,
+		Ingredients: []string{
+			"water",
+			"carrot",
+			"cabbage",
+		},
+	}
+
+	image := make([]byte, 60)
+
+	dish, err := suite.service.Create(context.Background(), &createInfo, image)
+
+	suite.Nilf(err, "DishService.Create shouldn't return an error, when input is valid")
+
+	if suite.NotNil(dish, "DishService.Create should not return nil dish, when everything is valid") {
+		suite.Equalf(dish.Name(), createInfo.Name, "DishService.Create expected dish name %s, got %s", createInfo.Name, dish.Name())
+		suite.Equalf(dish.Price(), createInfo.Price, "DishService.Create expected dish price %f. got %f", createInfo.Price, dish.Price())
+		suite.ElementsMatch(dish.Ingredients, createInfo.Ingredients, "DishService.Create ingredients should be the same as in createInfo")
+		suite.Equal(dish.Image(), validUrl, "DishService.Create should have url from the ImageStorage")
+	}
 }
 
 func TestDishService(t *testing.T) {
